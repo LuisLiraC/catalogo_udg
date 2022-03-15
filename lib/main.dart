f5c23e500c8d5342e081ed9ee1f6de75503df4b0
LuisLiraC <luis_ssbm@hotmail.com>
2022-03-14 22:05:47 -0600

    3.6. Peticiones HTTP

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,44 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Pokemon> fetchPost(int pokemonId) async {
+  final response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/$pokemonId'));
+
+  if (response.statusCode == 200) {
+    return Pokemon.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
+  }
+}
+
+class Pokemon {
+  final String name;
+  final int order;
+
+  Pokemon({
+    required this.name,
+    required this.order
+  });
+
+  factory Pokemon.fromJson(Map<String, dynamic> json) {
+    return Pokemon(
+      name: json['name'],
+      order: json['order'],
+    );
+  }
+}
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp(pokemon: fetchPost(400)));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final Future<Pokemon> pokemon;
+
+  const MyApp({Key? key, required this.pokemon}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +66,18 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                   fontSize: 20.0,
                 ),
+              ),
+              FutureBuilder<Pokemon>(
+                  future: pokemon,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data!.name);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+
+                    return const CircularProgressIndicator();
+                  }
               )
             ],
           ),
